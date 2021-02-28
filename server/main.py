14a713746181fc95a749ca201e1b68fe0af2eb65
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-02-28 17:49:03 +0100

    Fixed name of the database file

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from Wrapper import PDBC
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    db = PDBC('database.db')  # Connect/Create the database
+    db = PDBC('data.db')  # Connect/Create the database
     atexit.register(db.close)  # On clean exit, close the connections
 
     app.run(host="0.0.0.0", port=8000)  # Use for testing
