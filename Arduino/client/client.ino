ab50e089ad29b41f71ce019efa883348c68def4d
inoricat <ivan@inori.es>
2021-03-02 14:41:30 +0100

    HTTP connection functions (untested)

--- Arduino/client/client.ino
+++ Arduino/client/client.ino
@@ -1,8 +1,42 @@
+#include "esp_http_client.h"
+#include <string.h>
+#define DATA_MAX_SIZE 512
 
+
+esp_err_t err;
+const esp_http_client_config_t config = {
+  .url = "http://192.168.0.100/"
+};
+esp_http_client_handle_t connHandle;
+
+
+char *data;
+int dataLen;
+
+void initHTTPConnection() {
+  connHandle = esp_http_client_init(&config);
+  esp_http_client_set_method(connHandle, HTTP_METHOD_POST);
+  
+}
+
+/*
+ * Make POST request with data on data variable.
+ */
+void sendPOST() {
+  dataLen = strlen(data);
+  esp_http_client_set_method(connHandle, HTTP_METHOD_POST);
+  esp_http_client_set_post_field(connHandle, data, dataLen);
+  err = esp_http_client_perform(connHandle);
+  Serial.printf("Request made, err: %d", err);
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // set serial for usb printing
+  data = malloc(sizeof(char)*DATA_MAX_SIZE);
+  dataLen = 0;
+  
+  initHTTPConnection();
   
 }
 
