ea1629120e9835779e7675a11726ed1668c73278
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-03-30 23:58:35 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,7 +1,6 @@
 import atexit
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify
 import requests
-
 import Wrapper
 from Exceptions import UnregisteredDeviceException
 from Wrapper import PDBC, Log
@@ -11,24 +10,36 @@ app = Flask(__name__)
 app.use_reloader = False
 log = Log("requests.log")
 db = PDBC('data.db')  # Connect/Create the database
-server_dir=""
+server_dir = ""
 shard = 0
 
-# PROTOCOL
-#machine\ntype,value\ntype,value\ntype,value
+
+@app.route("/test", methods=["GET"])
+def test():
+    data = "0,\nint,3\nint,4"
+    r = requests.post("localhost:8000/receive_data", params=data)
+
+    return r
 
 
+# PROTOCOL LAN
 @app.route("/receive_data", methods=["POST"])
 def receive_data():
+    # machine\ntype,value\ntype,value\ntype,value
+
     dataReceived = request.headers
     machine = dataReceived.pop(0)
     for line in dataReceived:
-        resend_data(Wrapper.timestamp(), machine, shard, "", line.split(",")[1], "WIFI" )
-    pass
+        valueType = line.split(",")[0]
+        value = line.split(",")[1]
+        resend_data(Wrapper.timestamp(), machine, "", value, "WIFI")
+        log.log("Machine " + machine + " sent an " + valueType + " with value " + value)
+
+    return 200
 
 
 # Protocol WAN
-def resend_data(timestamp, machine, shard, sensorName, value, net):
+def resend_data(timestamp, machine, sensorName, value, net):
     data = {
         [{"timestamp": timestamp,
           "machine": machine,
@@ -37,10 +48,10 @@ def resend_data(timestamp, machine, shard, sensorName, value, net):
           "value": value,
           "net": net
           }]
-        }
-
+    }
 
     r = requests.post(server_dir, params=data)
+
 
 @app.route("/getid", methods=["GET"])
 def get_id():
