4487e6b7a6933274f5a93ffb3a1c5fb2d0589b1f
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-04-26 23:10:59 +0200

    If the database does not exist, it is created from the sql file (#11)
    * Resend to the server if its address is defined
    
    * Finally! If the database does not exist, create it from the sql file

--- server/main.py
+++ server/main.py
@@ -11,36 +11,43 @@ app = Flask(__name__)
 app.use_reloader = False
 log = Log("requests.log")
 db = PDBC('data.db')  # Connect/Create the database
-server_dir=""
+server_dir = ""
 shard = 0
 
-# PROTOCOL
-#machine\ntype,value\ntype,value\ntype,value
 
-
+# PROTOCOL LAN
 @app.route("/receive_data", methods=["POST"])
 def receive_data():
-    dataReceived = request.headers
+    # machine\ntype,value\ntype,value\ntype,value
+
+    dataReceived = str(request.data, 'utf-8').split("\n")
     machine = dataReceived.pop(0)
     for line in dataReceived:
-        resend_data(Wrapper.timestamp(), machine, shard, "", line.split(",")[1], "WIFI" )
-    pass
+        valuetype = line.split(",")[0]
+        value = line.split(",")[1]
+
+        if server_dir != "":
+            resend_data(Wrapper.timestamp(), machine, valuetype, value, "WIFI")
+
+        log.log("The device " + machine + " sent the value " + value + " of type " + valuetype)
+
+    return "OK"
 
 
 # Protocol WAN
-def resend_data(timestamp, machine, shard, sensorName, value, net):
-    data = {
-        [{"timestamp": timestamp,
-          "machine": machine,
-          "shard": shard,
-          "type": sensorName,
-          "value": value,
-          "net": net
-          }]
-        }
+def resend_data(timestamp, machine, sensorName, value, net):
+    data = [{
+        "timestamp": timestamp,
+        "machine": machine,
+        "shard": shard,
+        "type": sensorName,
+        "value": value,
+        "net": net
+    }]
 
+    r = requests.post(server_dir, json=json.dumps(data))
+    return r.status_code
 
-    r = requests.post(server_dir, params=data)
 
 @app.route("/getid", methods=["GET"])
 def get_id():
@@ -64,6 +71,7 @@ def close():  # This function will execute on clean exit
     db.close()  # Close the database connections
     log.log("Server is shutting down.")  # Close the log file
     log.close()  # Close the log file
+    print("Database and log closed successfully")
 
 
 if __name__ == '__main__':
