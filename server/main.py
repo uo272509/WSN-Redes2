fd0490ec986a6daa5edb931b84793f9b22e90d79
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-04-26 17:53:53 +0200

    Marcos (#10)
    * Silvia (#6)
    
    * Creación de database.db e intento de insertar cosas (no funciona)
    
    * Fixed database file
    
    * Fixed query to the database
    
    * Fixed name of the database file
    
    * funciones de db ( no funciona, error de threads)
    
    * Fixed database connection (multithreads) and a couple more things
    
    * Servidor Lan- [POST] data (send to central server) DONE
    
    Co-authored-by: Marcos Gutiérrez Alonso <margual56@gmail.com>
    
    * Marcos's changes (#7)
    
    * Added python to the gitignore
    
    * Initial setup for servers
    
    * Create requirements.txt
    
    * Created the ID request function and the Log class
    
    I need the database to complete it
    
    * Corrected a comment
    
    * Fixed datetime formatting
    
    * Fixed error of logging
    
    * Moved atexit things to one function
    
    * Fixed exception constructor
    
    * Silvia (#8)
    
    * Creación de database.db e intento de insertar cosas (no funciona)
    
    * Fixed database file
    
    * Fixed query to the database
    
    * Fixed name of the database file
    
    * funciones de db ( no funciona, error de threads)
    
    * Fixed database connection (multithreads) and a couple more things
    
    * Servidor Lan- [POST] data (send to central server) DONE
    
    * primet intento de data(receive)
    
    Co-authored-by: Marcos Gutiérrez Alonso <margual56@gmail.com>
    
    * Changes from my laptop(?) (#9)
    
    * Added python to the gitignore
    
    * Initial setup for servers
    
    * Create requirements.txt
    
    * Created the ID request function and the Log class
    
    I need the database to complete it
    
    * Corrected a comment
    
    * Fixed datetime formatting
    
    * Fixed error of logging
    
    * Moved atexit things to one function
    
    * Fixed exception constructor
    
    * LAN y WAN funcionando
    
    * Update main.py
    
    Co-authored-by: Silvia Rodríguez Bares <67699345+siroba@users.noreply.github.com>

--- server/main.py
+++ server/main.py
@@ -1,13 +1,50 @@
 import atexit
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
+import requests
 
+import Wrapper
 from Exceptions import UnregisteredDeviceException
 from Wrapper import PDBC, Log
 
 app = Flask(__name__)
-app.use_reloader=False
+
+app.use_reloader = False
 log = Log("requests.log")
 db = PDBC('data.db')  # Connect/Create the database
+server_dir = ""
+shard = 0
+
+
+# PROTOCOL LAN
+@app.route("/receive_data", methods=["POST"])
+def receive_data():
+    # machine\ntype,value\ntype,value\ntype,value
+
+    dataReceived = str(request.data,'utf-8').split("\n")
+    machine = dataReceived.pop(0)
+    for line in dataReceived:
+        valuetype = line.split(",")[0]
+        value = line.split(",")[1]
+
+        resend_data(Wrapper.timestamp(), machine, "", value, "WIFI")
+        log.log("The device " + machine + " sent the value " + value + " of type " + valuetype)
+
+    return "OK"
+
+
+# Protocol WAN
+def resend_data(timestamp, machine, sensorName, value, net):
+    data = [{
+          "timestamp": timestamp,
+          "machine": machine,
+          "shard": shard,
+          "type": sensorName,
+          "value": value,
+          "net": net
+          }]
+
+    # r = requests.post(server_dir, params=json.dumps(data))
+    return "OK"
 
 
 @app.route("/getid", methods=["GET"])
